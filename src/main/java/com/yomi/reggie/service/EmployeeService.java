88cad09ab3bef2710bca3a5a9efc34204f0edc20
ChealseaYao <successsssssss1126@outlook.com>
2024-07-09 20:20:31 +0930

    employee

---
+++ src/main/java/com/yomi/reggie/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.yomi.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yomi.reggie.entity.Employee;
+
+public interface EmployeeService extends IService<Employee> {
+}
